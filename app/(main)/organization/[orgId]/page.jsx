96b1aa796dcd8edff7a17fbbc1c4a89083877b2b
abhilashdk2016 <abhilashdk2014@outlook.com>
2024-12-25 22:48:40 +0530

    escape character

--- app/(main)/organization/[orgId]/page.jsx
+++ app/(main)/organization/[orgId]/page.jsx
@@ -22,7 +22,7 @@ const Organization = async ({ params }) => {
   return (
     <div className='container mx-auto'>
       <div className='mb-4 flex flex-col sm:flex-row justify-between items-start'>
-        <h1 className='text-5xl font-bold gradient-title pb-2'>{organization.name}'s Projects</h1>
+        <h1 className='text-5xl font-bold gradient-title pb-2'>{organization.name}&apos;s Projects</h1>
         <OrgSwitcher />
       </div>
       <div className='mb-4'>
